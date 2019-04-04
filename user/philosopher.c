f6c43201d1ede97df7298bd1a3fe0bea79a8c522
Angus Parsonson <ap17691@it025976.wks.bris.ac.uk>
2019-04-04 16:23:12 +0100

    adding a cheeky marksheety boi lol

---
+++ user/philosopher.c
@@ -0,0 +1,58 @@
+#include "philosopher.h"
+
+int spork[16] = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
+int mutex     = 1;
+
+// Updates array to say sporks are in use
+void pick_up_sporks(int id) {
+  sem_wait(&spork[id]);
+  sem_wait(&spork[(id + 1) % 16]);
+}
+
+// Updates array to say sporks are no longer in use
+void put_down_sporks(int id) {
+  sem_post(&spork[id]);
+  sem_post(&spork[(id + 1) % 16]);
+}
+
+void main_philosopher() {
+
+  for (int i = 0; i < 16; i++) {
+    if (fork() == 0) { // Section that controls all the fork children
+      int philosopher_id = i;
+      char c[2];
+      itoa(c, philosopher_id);
+
+      while( 1 ) {
+
+        // Philosopher thinks
+        write( STDOUT_FILENO, "Philosopher ", 12 );write( STDOUT_FILENO, c, 2 );write( STDOUT_FILENO, " is thinking \n", 14 );
+        sleep();
+        sem_wait(&mutex);
+
+        // Critical section start
+        if ( spork[philosopher_id] && spork[(philosopher_id + 1) % 16] ) {
+          // Philosopher picks up sporks
+          pick_up_sporks(philosopher_id);
+          sem_post(&mutex); // Critical section end
+
+          // Philosopher eats
+          write( STDOUT_FILENO, "Philosopher ", 12 );write( STDOUT_FILENO, c, 2 );write( STDOUT_FILENO, " is eating \n", 12 );
+          sleep();
+
+          write( STDOUT_FILENO, "Philosopher ", 12 );write( STDOUT_FILENO, c, 2 );write( STDOUT_FILENO, " puts his cutlery down \n", 24 );
+          // Philosopher waits for lock to put down sporks
+          sem_wait(&mutex);
+
+          // Critical section start
+          put_down_sporks(philosopher_id);
+        }
+        
+        sem_post(&mutex); // Critical section end
+
+      }
+    }
+  }
+
+  exit( EXIT_SUCCESS );
+}
